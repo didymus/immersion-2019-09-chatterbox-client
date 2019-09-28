eff17814d037efd0664b6cf5ba306e34d7c8a607
thomasmudge <alephcult@gmail.com>
2019-09-28 12:32:05 -0500

    A little more rooms.js functionality

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,5 +1,22 @@
 const Rooms = {
+    const Rooms = {
+        
+        _data: new Set,
+        selected: 'lobby',
 
+        items: function () {
+            return _.chain([...Rooms._data]);
+        },
+
+        isSelected: function (roomname) {
+            return roomname === Rooms.selected || !roomname && Rooms.selected === 'lobby';
+        },
+
+        add: function (room, callback = () => { }) {
+            Rooms._data.add(room);
+            Rooms.selected = room;
+            callback(Rooms.items());
+        },
 
 
 };
