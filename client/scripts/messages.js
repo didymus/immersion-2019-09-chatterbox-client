fc5af867949289a7a8f6eb6a6bb0cc219dd5d1bb
thomasmudge <alephcult@gmail.com>
2019-09-28 15:15:06 -0500

    Messages

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,5 +1,35 @@
 const Messages = {
+_data: {},
 
+items: function () {
+return _.chain(Object.values(Messages._data)).sortBy('createdAt');
+},
+
+add: function (message, callback = () => { }) {
+Messages._data[message.objectId] = message;
+callback(Messages.items());
+},
+
+update: function (messages, callback = () => { }) {
+const length = Object.keys(Messages._data).length;
+
+for (let message of messages) {
+Messages._data[message.objectId] = Messages._conform(message);
+}
+// invoke callback if changed
+if(Object.keys(Messages._data).length !== length) {
+callback(Messages.items());
+}
+},
+
+_conform: function (message) {
+
+//console.log(message);
+message.text = message.text || '';
+message.username = message.username || '';
+message.roomname = message.roomname || '';
+return message;
+},
 
 };
 
